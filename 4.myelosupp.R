5ab7ebbdeb1629e6cb94f26038424ad32d573202
chrisleitzinger <christelleleitzinger@gmail.com>
2020-07-14 10:16:58 -0400

    Plots for CBC

--- 4.myelosupp.R
+++ 4.myelosupp.R
@@ -1,11 +1,6 @@
 # Blood
 
 
-ggplot(global_data, aes(x=Case_Control, y=ChangeANC, fill=CHIP))+
-  geom_bar(stat = "summary_bin")
-
-min(global_data$ChangeANC, na.rm = TRUE)
-
 # boxplot
 ggplot(global_data, aes(x=CHIP, y=BaseANC, fill=Case_Control))+
   geom_boxplot()
@@ -65,6 +60,53 @@ global_data %>% gather("blood_subset", "value", c("BaseANC", "ChangeANC",
   facet_wrap(. ~ blood_subset, scales = "free",  ncol=2)+
   stat_compare_means(aes(group = Case_Control))
 
+global_data %>% gather("blood_subset", "value", c("BaseANC", "ChangeANC",
+                                                  "BaseHGB", "ChangeHGB",
+                                                  "BasePLT", "ChangePLT",
+                                                  "BaseWBC", "ChangeWBC")) %>% 
+  select("NGS_ID", "blood_subset", "value", "CHIP", "Case_Control") %>% 
+  mutate(blood_subset= factor(blood_subset, levels = c("BaseANC", "ChangeANC",
+                                                       "BaseHGB", "ChangeHGB",
+                                                       "BasePLT", "ChangePLT",
+                                                       "BaseWBC", "ChangeWBC"))) %>% 
+  ggplot(aes(x=Case_Control, y=value, fill=CHIP))+
+  geom_boxplot()+
+  #geom_point(data = df2, aes(x = CHIP, y = value, fill=Case_Control), colour = "white", alpha=0) +
+  facet_wrap(. ~ blood_subset, scales = "free",  ncol=2)+
+  stat_compare_means(aes(group = CHIP))
+
+global_data %>% gather("blood_subset", "value", c("ChangeANC","ChangeHGB","ChangePLT","ChangeWBC")) %>% 
+  select("NGS_ID", "blood_subset", "value", "CHIP", "Case_Control") %>% 
+  ggplot(aes(x=CHIP, y=value, fill=Case_Control))+
+  geom_bar(stat = "summary_bin")+
+  facet_wrap(. ~ blood_subset, scales = "free",  ncol=2)+
+  stat_compare_means(aes(group = CHIP), method = "anova", label.y= 0)
+min(global_data$ChangeANC, na.rm = TRUE)
+max(global_data$ChangeANC, na.rm = TRUE)
+mean(global_data$ChangeANC, na.rm = TRUE)
+
+global_data %>% gather("blood_subset", "value", c("BaseANC","BaseHGB","BasePLT","BaseWBC")) %>% 
+  select("NGS_ID", "blood_subset", "value", "CHIP", "Case_Control") %>% 
+  ggplot(aes(x=CHIP, y=value, fill=Case_Control))+
+  geom_bar(stat = "summary_bin")+
+  facet_wrap(. ~ blood_subset, scales = "free",  ncol=2)+
+  stat_compare_means(aes(group = CHIP), method = "t.test", label.y= 0)+
+  stat_compare_means(label.y= 5)
+
+
+
+
+
+
+# Logistic regression----
+# Case prediction by CHIP
+
+
+
+
+
+
+# Strata----
 global_data %>% 
   ggpaired(x = "Case_Control", y = "BaseANC",
            id = "Strata",
