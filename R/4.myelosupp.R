b90d2131974552761fa71d27e9b60d205ff6fcd4
chrisleitzinger <christelleleitzinger@gmail.com>
2020-07-20 13:21:25 -0400

    Move script

--- R/4.myelosupp.R
+++ R/4.myelosupp.R
@@ -91,7 +91,39 @@ global_data %>% gather("blood_subset", "value", c("BaseANC", "ChangeANC",
 # tbl_merge(list(tbl1, tbl2),
 #                  tab_spanner = c("**CHIP**", "**no CHIP**"))  %>% as_gt()
 # 
-
+p1 <- global_data %>% gather("blood_subset", "value", c("BaseANC", "ChangeANC",
+                                                        "BaseHGB", "ChangeHGB",
+                                                        "BasePLT", "ChangePLT",
+                                                        "BaseWBC", "ChangeWBC")) %>% 
+  select("NGS_ID", "blood_subset", "value", "CHIP", "Case_Control") %>% 
+  mutate(blood_subset= factor(blood_subset, levels = c("BaseANC", "ChangeANC",
+                                                       "BaseHGB", "ChangeHGB",
+                                                       "BasePLT", "ChangePLT",
+                                                       "BaseWBC", "ChangeWBC"))) %>% 
+  ggplot(aes(x=Case_Control, y=value))+
+  geom_boxplot()+
+  facet_wrap(. ~ blood_subset, scales = "free",  ncol=2, strip.position = "right")+
+  stat_compare_means(size= 3)
+df2 <- data.frame(CHIP = rep("CHIP",4), blood_subset = factor(c("BaseANC", "ChangeANC",
+                                                                "BaseHGB", "ChangeHGB",
+                                                                "BasePLT", "ChangePLT",
+                                                                "BaseWBC", "ChangeWBC")),
+                  value = c(10,10,16,NA, NA, NA, 10,10), Case_Control = rep("Cases",4))
+p2 <- global_data %>% gather("blood_subset", "value", c("BaseANC", "ChangeANC",
+                                                        "BaseHGB", "ChangeHGB",
+                                                        "BasePLT", "ChangePLT",
+                                                        "BaseWBC", "ChangeWBC")) %>% 
+  select("NGS_ID", "blood_subset", "value", "CHIP", "Case_Control") %>% 
+  mutate(blood_subset= factor(blood_subset, levels = c("BaseANC", "ChangeANC",
+                                                       "BaseHGB", "ChangeHGB",
+                                                       "BasePLT", "ChangePLT",
+                                                       "BaseWBC", "ChangeWBC"))) %>% 
+  ggplot(aes(x=CHIP, y=value, fill=Case_Control))+
+  geom_boxplot()+
+  geom_point(data = df2, aes(x = CHIP, y = value, fill=Case_Control), colour = "white", alpha=0) +
+  facet_wrap(. ~ blood_subset, scales = "free",  ncol=2, strip.position = "right")+
+  stat_compare_means(size= 3)
+gridExtra::grid.arrange(p1, p2, ncol = 2, widths= c(4,6))
 
 # What if we compare Case_Control
 global_data %>% gather("blood_subset", "value", c("BaseANC", "ChangeANC",
